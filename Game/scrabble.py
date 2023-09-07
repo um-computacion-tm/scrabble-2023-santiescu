e864b8bf8ebbcf0339b931f3c1c793ebbb7af0f3
santi <sb.escudero@gmail.com>
2023-09-07 19:42:29 -0300

    add scrabble

---
+++ Game/scrabble.py
@@ -0,0 +1,23 @@
+from Game.board import Board
+from Game.player import Player
+from Game.models import BagTiles
+
+
+class ScrabbleGame:
+    def __init__(self, players_count: int):
+        self.board = Board()
+        self.bag_tiles = BagTiles()
+        self.players:list[Player] = []
+        for _ in range(players_count):
+            self.players.append(Player(bag_tiles=self.bag_tiles))
+        
+        self.current_player = None
+
+    def next_turn(self):
+        if self.current_player is None:
+            self.current_player = self.players[0]
+        else:
+            #index = index del current player + 1
+            #len(self.players)
+            index = self.players.index(self.current_player) + 1
+            self.current_player = self.players[index]
---
+++ Test/test_scrabble.py
@@ -0,0 +1,44 @@
+import unittest
+from Game.scrabble import ScrabbleGame
+
+
+class TestScrabbleGame(unittest.TestCase):
+    def test_init(self):
+        scrabble_game = ScrabbleGame(players_count=3)
+        self.assertIsNotNone(scrabble_game.board)
+        self.assertEqual(
+            len(scrabble_game.players),
+            3,
+        )
+        self.assertIsNotNone(scrabble_game.bag_tiles)
+    
+    def test_next_turn_when_game_is_starting(self):
+        #Validar que al comienzo, el turno es del jugador 0
+        scrabble_game = ScrabbleGame(players_count=3)
+
+        scrabble_game.next_turn()
+
+        assert scrabble_game.current_player == scrabble_game.players[0]
+
+
+    def test_next_turn_when_player_is_not_the_first(self):
+        #Validar que luego del jugador 0, le toca al jugador 1
+        scrabble_game = ScrabbleGame(players_count=3)
+        scrabble_game.current_player = scrabble_game.players[0]
+
+        scrabble_game.next_turn()
+
+        assert scrabble_game.current_player == scrabble_game.players[1]
+
+    def test_next_turn_when_player_is_last(self):
+        #Suponiendo que tenemos 3 jugadores, luego del jugador 3, le toca al jugador 1
+        scrabble_game = ScrabbleGame(players_count=3)
+        scrabble_game.current_player = scrabble_game.players[2]
+
+        scrabble_game.next_turn()
+
+        assert scrabble_game.current_player == scrabble_game.players[0]
+
+
+if __name__ == '__main__':
+    unittest.main()
