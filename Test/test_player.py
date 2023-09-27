181628f6da31f996b072ed8d06b9363c0a0d283d
santi <sb.escudero@gmail.com>
2023-09-27 09:29:01 -0300

    add test player

--- Test/test_player.py
+++ Test/test_player.py
@@ -1,17 +1,60 @@
 import unittest
-from Game.player import Player 
+from Game.player import Player
+from Game.models import Tile, BagTiles
 
-class TestPlayer(unittest.TestCase)
+class TestPlayer(unittest.TestCase):
     def test_init(self):
-        board = board()
+        player_1 = Player()
         self.assertEqual(
-            len(board.grid),
-            15,
+            len(player_1.tiles),
+            0,
         )
-        self.assertEqual(
-            len(board.grid [0]),
-            15,
-        )
+
+    def test_validate_user_has_letters(self):
+        bag_tile = BagTiles()
+        bag_tile.tiles = [
+            Tile(letter='H', value=1),
+            Tile(letter='O', value=1),
+            Tile(letter='L', value=1),
+            Tile(letter='A', value=1),
+            Tile(letter='C', value=1),
+            Tile(letter='U', value=1),
+            Tile(letter='M', value=1),
+        ]
+        player = Player(bag_tile)
+        tiles = [
+            Tile(letter='H', value=1),
+            Tile(letter='O', value=1),
+            Tile(letter='L', value=1),
+            Tile(letter='A', value=1),
+        ]
+
+        is_valid = player.has_letters(tiles)
+
+        self.assertEqual(is_valid, True)
+
+        def test_validate_fail_when_user_has_not_letters(self):
+            bag_tile = BagTiles()
+        bag_tile.tiles = [
+            Tile(letter='P', value=1),
+            Tile(letter='O', value=1),
+            Tile(letter='L', value=1),
+            Tile(letter='A', value=1),
+            Tile(letter='C', value=1),
+            Tile(letter='U', value=1),
+            Tile(letter='M', value=1),
+        ]
+        player = Player(bag_tile)
+        tiles = [
+            Tile(letter='H', value=1),
+            Tile(letter='O', value=1),
+            Tile(letter='L', value=1),
+            Tile(letter='A', value=1),
+        ]
+
+        is_valid = player.has_letters(tiles)
+
+        self.assertEqual(is_valid, False)
 
 if __name__ == '__main__':
     unittest.main()
