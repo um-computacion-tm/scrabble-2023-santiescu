0be341b7a886e24be24050e2bd64f9e67c7478a0
santi <sb.escudero@gmail.com>
2023-10-06 19:52:38 -0300

    modified board

--- Game/board.py
+++ Game/board.py
@@ -6,3 +6,28 @@ class Board:
             [Cell(1,'') for _ in range(15)]
             for _ in range (15)
         ]
+
+    @staticmethod
+    def calculate_word_value(word: list[Cell]) -> int:
+        value: int = 0
+        multiplier_word = None
+        for cell in word:
+            value = value + cell.calculate_value()
+            if cell.multiplier_type == "word" and cell.active:
+                multiplier_word = cell.multiplier
+        if multiplier_word:
+            value = value * multiplier_word
+        return value
+
+    def validate_word_inside_board(self, word, location, orientation):
+        #WORK IN PROGRESS
+        position_x = location[0]
+        position_y = location[1]
+        len_word = len(word)
+        if orientation == "H":
+            if position_x + len_word > 15:
+                return False
+            else:
+                return True
+        else:
+            pass
