42c3d1c7d7b86ba8993af6de4be56cf4fb89c3d1
santi <sb.escudero@gmail.com>
2023-10-08 18:21:24 -0300

    change player

--- Game/player.py
+++ Game/player.py
@@ -1,3 +1,13 @@
+from Game.models import BagTiles
+
 class Player:
-    def __init__(self, bag_tiles):
-        self.tiles = bag_tiles (7) 
+    def __init__(self, id:int, name = None):
+        self.id = id
+        bag = BagTiles()
+        self.tiles = bag.take(7)
+        self.score = 0
+
+
+    def set_name(self, name):
+        self.name = name
+        return name
