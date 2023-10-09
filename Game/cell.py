7901cc854c9e993461b3736f3fa5a43d5e1ab021
santi <sb.escudero@gmail.com>
2023-10-09 14:18:18 -0300

    change cell

--- Game/cell.py
+++ Game/cell.py
@@ -2,18 +2,21 @@ from Game.models import Tile
 
 class Cell:
     
-    def __init__(self, multiplier, multiplier_type, letter):
+    def __init__(self, multiplier=1, multiplier_type="", letter=None, word= None):
         self.multiplier= multiplier
         self.multiplier_type = multiplier_type
-        self.letter = None
+        self.letter = letter 
+        self.word = word
 
-    def add_letter(self, letter): 
+    def add_letter(self, letter:Tile): 
         self.letter=letter 
 
     def calculate_value(self):
         if self.letter is None:
             return 0
         if self.multiplier_type == 'letter':
+            value = self.letter.value * self.multiplier
+            self.multiplier_type = None
             return self.letter.value * self.multiplier
         else:
             return self.letter.value
@@ -21,11 +24,11 @@ class Cell:
     def calculate_word_value(self, word):
             word_value = 0
             word_multiplier = 1
-            for l in word :
-                word_value += l.calculate_value()  
-                if l.multiplier_type == 'word':
-                    word_multiplier = l.multiplier
-                    l.multiplier_type = None
+            for letter in word:
+                word_value += letter.calculate_value()
+            if letter.multiplier_type == 'word':
+                word_multiplier = letter.multiplier
+                letter.multiplier_type = None
             word_value *= word_multiplier
             return word_value
 
